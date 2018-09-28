68d7b001094224e50b6b31d48c67aeae732fa1a2
Jackie Balzer <jackie@behance.com>
2018-09-27 21:07:39 -0500

    ghetto responsive dom dance

--- js/site.js
+++ js/site.js
@@ -2,11 +2,37 @@ window.onscroll = function() {
   let siteHeader = document.getElementsByClassName('js-siteHeader')[0];
 };
 
-let media = window.matchMedia('(max-width: 980px)');
+let media = window.matchMedia('(max-width: 910px)');
+let contents = [];
+let desktop = document.getElementsByClassName('js-desktop-contents');
+let mobile = document.getElementsByClassName('js-mobile-contents');
+let map = Array.prototype.map;
 
-media.addListener((mq) => {
-
+map.call(desktop, function(el, i) {
+  contents[i] = el.innerHTML;
 });
+
+const swapContents = function(mq) {
+  if (mq.matches) {
+    map.call(desktop, function(el, i) {
+      el.innerHTML = '';
+    });
+    map.call(mobile, function(el, i) {
+      el.innerHTML = contents[i];
+    });
+  }
+  else {
+    map.call(mobile, function(el, i) {
+      el.innerHTML = '';
+    });
+    map.call(desktop, function(el, i) {
+      el.innerHTML = contents[i];
+    });
+  }
+}
+
+media.addListener(swapContents);
+swapContents(media);
 
 let toggleMobileNav = function() {
   mobileNav.classList.toggle('show');
