e52ca1259483d481d7d6a938185365c751a19c7e
Jackie Balzer <jackie@behance.com>
2018-09-27 19:22:32 -0500

    mobile nav

--- js/site.js
+++ js/site.js
@@ -1,3 +1,20 @@
 window.onscroll = function() {
   let siteHeader = document.getElementsByClassName('js-siteHeader')[0];
 };
+
+let media = window.matchMedia('(max-width: 980px)');
+
+media.addListener((mq) => {
+
+});
+
+let toggleMobileNav = function() {
+  mobileNav.classList.toggle('show');
+  mobileArrow.classList.toggle('swap');
+}
+
+let mobileArrow = document.getElementsByClassName('js-mobile-nav-arrow')[0];
+let mobileNav = document.getElementsByClassName('js-site-nav')[0];
+mobileArrow.addEventListener('touchstart', toggleMobileNav);
+mobileArrow.addEventListener('click', toggleMobileNav);
+
